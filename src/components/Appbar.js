6895b1cb16e46592fc95068b72eac6190e332748
IEeyaa <3200103483@zju.edu.cn>
2023-04-07 17:04:21 +0800

    20230407 IEeya 可以用了

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function ButtonAppBar() {
+export default function MyAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
--- src/pages/user/retrievePassword/retrievePassword.jsx
+++ src/pages/user/retrievePassword/retrievePassword.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
-function RetrievePassword() {
+export default function Retrivepassword() {
   return (
-    <div>
-      <h1>RetrievePassword Page</h1>
-    </div>
+    <center>
+      <br />
+      <Typography variant="h3" gutterBottom color={'#1976d2'}>
+        Retrive Password
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="standard-basic" label="E-mail address" variant="standard" />
+        <br/>
+        <Button size='small'>Get code</Button>
+        <br/>
+        <TextField id="standard-basic" label="Varification code" variant="standard" />
+        <br/>
+        <TextField id="standard-basic" label="New password" variant="standard" />
+        <br/>
+        <TextField id="standard-basic" label="Repeat password" variant="standard" />
+        <br/>
+        <Button>Submit</Button>
+      </Box>
+    </center>
   );
 }
-
-export default RetrievePassword;
