7607b73b62a551af29ca785692f47d1cc93d7b8b
IEeyaa <3200103483@zju.edu.cn>
2023-04-14 14:37:51 +0800

    20230414 IEeya 完成axios

--- src/pages/user/login/login.jsx
+++ src/pages/user/login/login.jsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
-
-import axios from 'axios';
+import { UserLogin } from '../../../services/api'
 
 function Login() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
   const postData = {
-    name: name,
+    mail: name,
     password: password,
   };
 
   const handleLogin = async () => {
-    axios.post('/user/login', postData)
-      .then(response => {
-        // 请求成功的处理
-        console.log(response.data);
-      })
-      .catch(error => {
-        // 请求失败的处理
-        console.error(error);
-      });
+    UserLogin(postData).then(res=>{
+      console.log(res);
+    });
   };
 
   return (
