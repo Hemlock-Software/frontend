58a3c07aaceb738781081c7a3563b2196de9bc28
Zou233 <928606715@qq.com>
2023-04-14 14:38:53 +0800

    zou:register

--- src/pages/user/register/register.jsx
+++ src/pages/user/register/register.jsx
@@ -1,11 +1,119 @@
-import React from "react";
+import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import { Button } from '@mui/material'
+import Typography from '@mui/material/Typography'
+
+import axios from 'axios'
 
 function Register() {
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [verifyCode, setVerifiedCode] = useState('')
+
+  function onMail(e) {
+    //console.log(e.target.value)
+    setMail(e.target.value)
+  }
+  function onPassword(e) {
+    //console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+  function onNickname(e) {
+    //console.log(e.target.value)
+    setNickname(e.target.value)
+  }
+  function onVerify(e) {
+    setVerifiedCode(e.target.value)
+  }
+
+  function submitMail() {
+    const postData = {
+      mail: mail,
+    }
+    console.log(postData)
+    axios
+      .post('/user/sendMail', postData)
+      .then((response) => {
+        // 请求成功的处理
+        console.log(response.data)
+      })
+      .catch((error) => {
+        // 请求失败的处理
+        console.error(error)
+      })
+  }
+
+  function submitRegister() {
+    const postData = {
+      mail: mail,
+      password: password,
+      nickname: nickname,
+      isManager: false,
+      verifyCode: verifyCode,
+    }
+    console.log(postData)
+    axios
+      .post('user/join', postData)
+      .then((response) => {
+        // 请求成功的处理
+        console.log(response.data)
+      })
+      .catch((error) => {
+        // 请求失败的处理
+        console.error(error)
+      })
+  }
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  );
+    <center>
+      <br />
+      <Typography variant="h3" gutterBottom color={'#1976d2'}>
+        Register
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off">
+        <TextField
+          id="standard-basic"
+          label="E-mail address"
+          variant="standard"
+          onChange={onMail}
+        />
+        <br />
+        <Button size="small" onClick={submitMail}>
+          Get Verified code
+        </Button>
+        <br />
+        <TextField
+          id="standard-basic"
+          label="verify code"
+          variant="standard"
+          onChange={onVerify}
+        />
+        <br></br>
+        <TextField
+          id="standard-basic"
+          label="password"
+          variant="standard"
+          onChange={onPassword}
+        />
+        <br />
+        <TextField
+          id="standard-basic"
+          label="nickname"
+          variant="standard"
+          onChange={onNickname}
+        />
+        <br />
+        <Button onClick={submitRegister}>Submit</Button>
+      </Box>
+    </center>
+  )
 }
 
-export default Register;
+export default Register
