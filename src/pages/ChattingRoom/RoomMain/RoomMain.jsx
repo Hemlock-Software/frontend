2ba9bc2a1eec340a1e3846d2620084a1502715ca
Dian-S <957085525@qq.com>
2023-06-09 12:53:28 +0800

    6.9 Shen fix some bugs

--- src/pages/ChattingRoom/RoomMain/RoomMain.jsx
+++ src/pages/ChattingRoom/RoomMain/RoomMain.jsx
@@ -489,7 +489,7 @@ function RoomMain() {
                     sx={{ mr: 1 }}
                     onChange={(e)=>setState({inputMessage: e.target.value})}
                   />
-                  <MuiFileInput label="Upload image" sx={{ mr: 1, width: "20%"}} value={value} onChange={handleChange} />
+                  <MuiFileInput placeholder="Upload image" sx={{ mr: 1, width: "32%"}} value={value} onChange={handleChange} />
                   <Button variant="contained" color="primary" onClick={send}>
                     Send
                   </Button>
